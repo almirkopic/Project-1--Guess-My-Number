b63bfe833988a6e49ef45324d57f857daecaeadb
Almir Kopic <133537874+almirkopic@users.noreply.github.com>
2023-10-28 17:10:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-'use strict';
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
 
-console.log(document.querySelector('.message').textCOntent);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+});
+
